64d9bf4fdbe7377a7fc018b826ef4ea19451f32f
ExoBlod <envyxwrath@outlook.com.pe>
2018-10-12 22:23:57 -0500

    prueba

---
+++ app/src/main/java/com/glitched/utp/furukawa/AllProductsFragment.java
@@ -0,0 +1,29 @@
+package com.glitched.utp.furukawa;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllProductsFragment extends Fragment {
+
+
+    public AllProductsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_products, container, false);
+    }
+
+}
